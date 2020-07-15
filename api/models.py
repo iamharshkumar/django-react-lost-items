d9b04ae51ce773503ae0657cac1c0d8fe52744e8
iamharshkumar <hk7206210@gmail.com>
2020-07-15 19:10:26 +0530

    add post and userprofile models

---
+++ api/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to='post', null=True)
+    type = models.CharField(max_length=10, choices=(('lost', 'lost'), ('found', 'found')))
+    active = models.BooleanField(default=False, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + ':' + self.name
